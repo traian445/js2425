b1b1a924b21dedd4481d4ad7c1f3914f954241d2
cateatraian <traiangrigoriu78@gmail.com>
2025-02-06 17:39:56 +0200

    commit

---
+++ curs4/recapitulare/listatari2.js
@@ -0,0 +1,104 @@
+const countriesData = [
+    {
+        "name": "South Georgia",
+        "capital": "King Edward Point",
+        "languages": [
+            "English"
+        ],
+        "population": 30,
+        "flag": "https://flagcdn.com/w320/gs.png",
+        "currency": "Saint Helena pound"
+    },
+    {
+        "name": "Grenada",
+        "capital": "St. George's",
+        "languages": [
+            "English"
+        ],
+        "population": 112519,
+        "flag": "https://flagcdn.com/w320/gd.png",
+        "currency": "Eastern Caribbean dollar"
+    },
+    {
+        "name": "Switzerland",
+        "capital": "Bern",
+        "languages": [
+            "French",
+            "Swiss German",
+            "Italian",
+            "Romansh"
+        ],
+        "population": 8654622,
+        "flag": "https://flagcdn.com/w320/ch.png",
+        "currency": "Swiss franc"
+    },
+    {
+        "name": "Sierra Leone",
+        "capital": "Freetown",
+        "languages": [
+            "English"
+        ],
+        "population": 7976985,
+        "flag": "https://flagcdn.com/w320/sl.png",
+        "currency": "Sierra Leonean leone"
+    },
+    {
+        "name": "Hungary",
+        "capital": "Budapest",
+        "languages": [
+            "Hungarian"
+        ],
+        "population": 9749763,
+        "flag": "https://flagcdn.com/w320/hu.png",
+        "currency": "Hungarian forint"
+    },
+    {
+        "name": "Taiwan",
+        "capital": "Taipei",
+        "languages": [
+            "Chinese"
+        ],
+        "population": 23503349,
+        "flag": "https://flagcdn.com/w320/tw.png",
+        "currency": "New Taiwan dollar"
+    },
+    {
+        "name": "Wallis and Futuna",
+        "capital": "Mata-Utu",
+        "languages": [
+            "French"
+        ],
+        "population": 11750,
+        "flag": "https://flagcdn.com/w320/wf.png",
+        "currency": "CFP franc"
+    },
+    {
+        "name": "Barbados",
+        "capital": "Bridgetown",
+        "languages": [
+            "English"
+        ],
+        "population": 287371,
+        "flag": "https://flagcdn.com/w320/bb.png",
+        "currency": "Barbadian dollar"
+    },
+    {
+        "name": "Pitcairn Islands",
+        "capital": "Adamstown",
+        "languages": [
+            "English"
+        ],
+        "population": 56,
+        "flag": "https://flagcdn.com/w320/pn.png",
+        "currency": "New Zealand dollar"
+    },
+    {
+        "name": "Ivory Coast",
+        "capital": "Yamoussoukro",
+        "languages": [
+            "French"
+        ],
+        "population": 26378275,
+        "flag": "https://flagcdn.com/w320/ci.png",
+        "currency": "West African CFA franc"
+    }]
